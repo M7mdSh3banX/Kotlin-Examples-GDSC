e36d679ec4d5bfde121f110c93181dd115d1597d
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-05-04 14:25:30 +0300

    implement for loop.kt

--- src/main/kotlin/10 for loop.kt
+++ src/main/kotlin/10 for loop.kt
@@ -1,3 +1,14 @@
 fun main() {
 
+    //Example 1
+    for (i in 1..10) {
+        println(i)
+    }
+
+    //Example 2
+    val cars = arrayOf("Volvo", "BMW", "Ford", "Mazda")
+    for (x in cars) {
+        println(x)
+    }
+    println(cars.joinToString("\n"))
 }
