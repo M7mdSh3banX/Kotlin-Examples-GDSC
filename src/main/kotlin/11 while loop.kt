d11a1f8e1e834ec38d558ed529df8f76cec32bc7
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-05-04 14:32:12 +0300

    implement while loop

---
+++ src/main/kotlin/11 while loop.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    var i = 0
+    while (i <= 10) {
+        println(i)
+        i++
+    }
+
+    val cars = arrayOf("Volvo", "BMW", "Ford", "Mazda")
+    var y = 0
+    while (y < cars.size) {
+        println(cars[y])
+        y++
+    }
+}
