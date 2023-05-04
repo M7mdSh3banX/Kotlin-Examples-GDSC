d1d71462e5e872848b1120e5ae37b0dfd2f2b67b
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-05-04 14:40:00 +0300

    implement do while loop

--- src/main/kotlin/12 do while loop.kt
+++ src/main/kotlin/12 do while loop.kt
@@ -2,16 +2,23 @@ fun main() {
 
     //Example 1
     var i = 0
-    while (i <= 10) {
+    do {
         println(i)
         i++
-    }
+    } while (i <= 10)
 
-    //Example 2
+    //Example 1
     val cars = arrayOf("Volvo", "BMW", "Ford", "Mazda")
     var y = 0
-    while (y < cars.size) {
+    do {
         println(cars[y])
         y++
-    }
+    } while (y < cars.size)
+
+    //Example 3
+    var z = 0
+    do {
+        println(z)
+        z++
+    } while (z == 5)
 }
